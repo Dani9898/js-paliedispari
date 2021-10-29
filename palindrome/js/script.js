84dee7ee0ba5c9c52aa0dec7cbd48c1dbb267c05
Dani9898 <danieleoliva98@gmail.com>
2021-10-29 16:44:10 +0200

    tolta variabile parola in piu

--- palindrome/js/script.js
+++ palindrome/js/script.js
@@ -7,14 +7,14 @@
 // Creare una funzione per capire se la parola inserita è palindroma. => function(parola) charAt(i) per selezionare un carattere di un parola e poi confrontare quel carattere se il primo con l'ultimo, se il secondo con il penutilmo etc... Se è pari tutto ok se è dispari bisogna che non conti il carattere in mezzo. quindi fare un controllo finche non arriva alla metà quidi => math.Floor della lunghezza della parola. Se nel confronto troviamo una disuguaglianza allora variabile booleana = false, se no rimane vera. Ritornara la variabile.
 
     function palindroma(parolaUser) {
-        let parola = parolaUser;
-        let lunghezza = parola.length;
+        // let parola = parolaUser;
+        let lunghezza = parolaUser.length;
         let medio = Math.floor(lunghezza / 2);
         let palindroma = true;
     
         for ( let i = 0; i < medio ; i++ ){
             
-            if (parola.charAt(i) != parola.charAt(lunghezza - 1 - i)) {
+            if (parolaUser.charAt(i) != parolaUser.charAt(lunghezza - 1 - i)) {
                 palindroma = false;
             }
     
