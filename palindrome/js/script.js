25a7e49667f362ed11f2e7468a9603b11e636d5f
Dani9898 <danieleoliva98@gmail.com>
2021-10-29 16:31:59 +0200

    es palindroma fatto

---
+++ palindrome/js/script.js
@@ -0,0 +1,31 @@
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma.
+// Dare l’output relativo.
+
+// chiedere all'utente di inserire una parola => prompt
+
+// Creare una funzione per capire se la parola inserita è palindroma. => function(parola) charAt(i) per selezionare un carattere di un parola e poi confrontare quel carattere se il primo con l'ultimo, se il secondo con il penutilmo etc... Se è pari tutto ok se è dispari bisogna che non conti il carattere in mezzo. quindi fare un controllo finche non arriva alla metà quidi => math.Floor della lunghezza della parola. Se nel confronto troviamo una disuguaglianza allora variabile booleana = false, se no rimane vera. Ritornara la variabile.
+
+    function palindroma(parolaUser) {
+        let parola = parolaUser;
+        let lunghezza = parola.length;
+        let medio = Math.floor(lunghezza / 2);
+        let palindroma = true;
+    
+        for ( let i = 0; i < medio ; i++ ){
+            
+            if (parola.charAt(i) != parola.charAt(lunghezza - 1 - i)) {
+                palindroma = false;
+            }
+    
+            return palindroma;   
+        }
+    
+    }
+
+    let parolaScelta = prompt("Inserisci parola");
+    if (palindroma(parolaScelta) === true) {
+        console.log("La parola che hai inserito è palindroma");
+    } else {
+        console.log("La parola che hai inserito non è palindroma");
+    }
