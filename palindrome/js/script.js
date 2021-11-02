85f453f7c6cb81c23b8920ba49d6a500ccf1f608
Dani9898 <danieleoliva98@gmail.com>
2021-11-02 11:31:58 +0100

    fix es pali sull'errore di parole come "ossoo"

--- palindrome/js/script.js
+++ palindrome/js/script.js
@@ -4,28 +4,33 @@
 
 // chiedere all'utente di inserire una parola => prompt
 
-// Creare una funzione per capire se la parola inserita è palindroma. => function(parola) charAt(i) per selezionare un carattere di un parola e poi confrontare quel carattere se il primo con l'ultimo, se il secondo con il penutilmo etc... Se è pari tutto ok se è dispari bisogna che non conti il carattere in mezzo. quindi fare un controllo finche non arriva alla metà quidi => math.Floor della lunghezza della parola. Se nel confronto troviamo una disuguaglianza allora variabile booleana = false, se no rimane vera. Ritornara la variabile.
+// Creare una funzione per capire se la parola inserita è palindroma. => function(parola) charAt(i) per selezionare un carattere di un parola e poi confrontare quel carattere  il primo con l'ultimo, il secondo con il penutilmo etc... Se è pari tutto ok se è dispari bisogna che non conti il carattere in mezzo. quindi fare un controllo finche non arriva alla metà quidi => math.Floor sulla lunghezza della parola. Se nel confronto troviamo una disuguaglianza allora variabile booleana = false, se no rimane vera. Ritornara la variabile.
 
     function palindroma(parolaUser) {
-        // let parola = parolaUser;
+
         let lunghezza = parolaUser.length;
         let medio = Math.floor(lunghezza / 2);
         let palindroma = true;
-    
+        
         for ( let i = 0; i < medio ; i++ ){
             
             if (parolaUser.charAt(i) != parolaUser.charAt(lunghezza - 1 - i)) {
                 palindroma = false;
-            }
-    
-            return palindroma;   
+                console.log(i);
+            }         
         }
-    
+
+        return palindroma;  
     }
 
     let parolaScelta = prompt("Inserisci parola");
+
     if (palindroma(parolaScelta) === true) {
         console.log("La parola che hai inserito è palindroma");
     } else {
         console.log("La parola che hai inserito non è palindroma");
     }
+
+
+
+  
